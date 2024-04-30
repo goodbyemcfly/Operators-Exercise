2d3d88a04ef59b9042f7ca034b366e2ab264910f
Brett Bellomy <brettbellomymusic@gmail.com>
2024-04-29 22:11:17 -0500

    first commit-- completed assignment

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,34 @@
 {
     public class Program
     {
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+
+        //Assignment #1
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            // Assignment #2
+
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with radius of {radius} is " + AreaOfCircle(radius) + "\n");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
         }
     }
 }
